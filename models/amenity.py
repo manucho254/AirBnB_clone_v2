12674c6ef3e3e5b241caa3527d9feee4352db490
manucho254 <manuchoadero@gmail.com>
2023-09-20 14:30:58 +0000

    updates to filestorage

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+
+from sqlalchemy import Column, String, ForeignKey
+
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Amenity class that defines amenities
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128))
