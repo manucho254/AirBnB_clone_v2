07aca6b92b77f328280e459ab2e29b7b3fb96a9a
manucho254 <manuchoadero@gmail.com>
2023-09-20 18:27:14 +0000

    minor code fixes

--- models/place.py
+++ models/place.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.amenity import association_table
 from os import getenv
 
 from sqlalchemy import Column, Integer, ForeignKey, Float, String
@@ -25,7 +24,7 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place", cascade="all, delete")
     amenities = relationship("Amenity",
-                             secondary=association_table,
+                             secondary='association_table',
                              back_populates="place_amenities",
                              viewonly=False
                              )
