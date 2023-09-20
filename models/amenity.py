bb3f44c8c1569d19a088e3c1e7d822b24e081f58
manucho254 <manuchoadero@gmail.com>
2023-09-20 20:20:12 +0000

    minor fixes on codebase

--- models/amenity.py
+++ models/amenity.py
@@ -7,13 +7,13 @@ from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id'),
-                                 primary_key=True),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id'),
-                                 primary_key=True)
-                          )
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True)
+                      )
 
 
 class Amenity(BaseModel, Base):
