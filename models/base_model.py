0dd20519efe5e7dfce2a08e71aaa715df4992098
manucho254 <manuchoadero@gmail.com>
2023-09-20 23:04:20 +0000

    fixes on BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -22,12 +22,8 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if len(kwargs) == 0 or kwargs is None:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            #self.updated_at = datetime.now()
 
-        else:
+        if kwargs:
             if 'id' not in kwargs:
                 self.id = str(uuid.uuid4())
 
@@ -52,6 +48,10 @@ class BaseModel:
                 del kwargs['__class__']
 
             self.__dict__.update(kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
