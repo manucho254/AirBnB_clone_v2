f7a9ac3bb516df31a821c6c42509298da1644180
manucho254 <manuchoadero@gmail.com>
2023-09-20 22:49:34 +0000

    fixes on BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
+        if len(kwargs) == 0 or kwargs is None:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
