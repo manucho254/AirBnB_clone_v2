4b4ede767d76c4e83c5a1c45010f75da23a58eaf
manucho254 <manuchodero@gmail.com>
2023-10-06 00:32:26 +0000

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,38 +20,9 @@ sudo ln -sf "$source_path" "$target_link"
 #change owner and group of path /data/ and all subpaths
 sudo chown -R ubuntu:ubuntu /data/
 
+my_alias="location /hbnb_static { alias /data/web_static/current/;}"
 #change default file
-echo "
-server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        add_header X-Served-By \$hostname;
-
-        # 404 error
-        error_page 404 /error404.html;
-
-        location / {
-                   # First attempt to serve request as file, then
-                   # as directory, then fall back to displaying a 404.
-                   try_files \$uri \$uri/ =404;
-        }
-        
-        location /redirect_me {
-                  return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-        }
-        
-        location /hbnb_static {
-                 # serve content in /data/web_static/current/;
-                 alias /data/web_static/current/;
-        }
-}" | sudo tee /etc/nginx/sites-available/default
+sed -i "s/server _;/server _;\n$my_alias" /etc/nginx/sites-available/default
 
 # restart nginx
 sudo service nginx restart
