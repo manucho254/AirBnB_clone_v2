e97cd8aca4f28ea434fb98a6efe0dd1b1847f304
manucho254 <manuchoadero@gmail.com>
2023-10-19 19:21:45 +0000

    puppet script to setup server

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,62 +1,65 @@
 # file that setups all directories in server
 
 exec { 'Kill apache2 process':
-    command => 'sudo killall apache2';
+    command => 'sudo killall apache2',
 }
 
 exec { 'Update and upgrade os':
-    command => 'sudo apt-get update -y && sudo apt-get upgrade -y';
+    command => 'sudo apt-get update -y && sudo apt-get upgrade -y',
 }
 
 exec { 'Install nginx':
-    command => 'sudo apt-get install nginx -y';
+    command => 'sudo apt-get install nginx -y',
 }
 
 exec { 'start nginx service':
-    command => 'sudo service nginx start';
+    command => 'sudo service nginx start',
 }
 
 exec { 'create all directories':
-    command => 'sudo mkdir -p /data/web_static/releases/test /data/web_static/shared';
+    command => 'sudo mkdir -p /data/web_static/releases/test /data/web_static/shared',
 }
 
 exec { 'create html file':
-    command => 'touch index.html';
+    command => 'touch index.html',
 }
 exec { 'create html file':
-    command => 'echo "Holberton Shchool" > index.html';
+    command => 'echo "Holberton Shchool" > index.html',
 }
 exec { 'create html file':
-    command => 'sudo mv index.html /data/web_static/releases/test/';
+    command => 'sudo mv index.html /data/web_static/releases/test/',
 }
 
 $source_path='/data/web_static/releases/test'
 $target_link='/data/web_static/current'
 
 exec { 'create default file':
-    command => "sudo rm -rf ${target_link}";
+    command => "sudo rm -rf ${target_link}",
 }
 
 exec { 'create a symbolic link':
-    command => "sudo ln -s ${source_path} ${target_link}";
+    command => "sudo ln -s ${source_path} ${target_link}",
 }
 
 exec { 'change owner and group of path /data/ and all subpaths':
-    command => 'sudo chown -R ubuntu:ubuntu /data/';
+    command => 'sudo chown -R ubuntu:ubuntu /data/',
 }
 
 exec { 'delete default files':
-    command => 'sudo rm -rf /etc/nginx/sites-available/default';
+    command => 'sudo rm -rf /etc/nginx/sites-available/default',
 }
 exec { 'delete default files':
-    command => 'sudo rm -rf /etc/nginx/sites-enabled/default';
+    command => 'sudo rm -rf /etc/nginx/sites-enabled/default',
 }
 
 exec { 'create default file':
-    command => 'touch default';
+    command => 'touch default',
 }
 
-$config='
+file { 'default':
+    ensure  => present,
+    path    => 'default',
+    content => '      
 server {
         listen 80 default_server;
 	listen [::]:80 default_server;
@@ -82,20 +85,18 @@ server {
 	location /redirect_me {
 	        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
 	}
-}'
-
-exec { 'add custom nginx config':
-    command => "echo ${$config} > default";
+}
+',
 }
 
 exec { 'move default file to /etc/nginx/sites-available/':
-    command => 'sudo mv default /etc/nginx/sites-available/default';
+    command => 'sudo mv default /etc/nginx/sites-available/default',
 }
 
 exec {'copy sites-available/default to sites-enabled/default':
-    command => 'sudo cp -r /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default';
+    command => 'sudo cp -r /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default',
 }
 
 exec { 'restart nginx':
-    command => 'sudo service nginx reload';
+    command => 'sudo service nginx reload',
 }
