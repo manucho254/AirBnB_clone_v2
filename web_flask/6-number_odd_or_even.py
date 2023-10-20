c46d4f9aff72342ae10e2579733bda16740bb939
manucho254 <manuchoadero@gmail.com>
2023-10-20 13:35:44 +0000

    display a HTML page only if n is an integer:

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+""" module for Flask web application
+"""
+from flask import Flask
+from flask import render_template
+
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def route_1():
+    """ script that starts a Flask web application:
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def route_2():
+    """ path to display “HBNB”
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def route_3(text):
+    """ display “C ” followed by the value of the text
+    """
+    value = escape(text).replace("_", " ")
+
+    return "C {}".format(value)
+
+
+@app.route("/python", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def route_4(text="is cool"):
+    """ display “Python ”, followed by the value of the text
+    """
+    value = escape(text).replace("_", " ")
+
+    return "Python {}".format(value)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def route_5(n):
+    """ display “n is a number” only if n is an integer
+    """
+
+    value = escape(n)
+
+    return "{} is a number".format(value)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def route_6(n):
+    """ display a HTML page only if n is an integer
+    """
+    return render_template("5-number.html", number=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def route_7(n):
+    """ display a HTML page only if n is an integer:
+        H1 tag: “Number: n is even|odd” inside the tag BODY
+    """
+
+    if n % 2 == 0:
+        data = {"number": n, "type": "even"}
+    else:
+        data = {"number": n, "type": "odd"}
+
+    return render_template("6-number_odd_or_even.html", data=data)
+
+
+if __name__ == "__main__":
+    app.run()
