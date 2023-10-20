a2e6a29d5c10b44f138b0b4598a50b02c19b49c0
manucho254 <manuchoadero@gmail.com>
2023-10-20 16:16:36 +0000

    flask app that lists all the states in /states_list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" module for Flask web application
+"""
+from flask import Flask
+from flask import render_template
+
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+storage.reload() # Reload objects
+states = storage.all(State) # Get all states
+
+
+@app.route("/states_list", strict_slashes=False)
+def status_list():
+    """ script that starts a Flask web application:
+    """
+
+    return render_template("7-states_list.html", states=states.values())
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ remove the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run()
