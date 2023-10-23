1b2709a4881f35b7b6c5b1e4cdc9cb277a3ae39f
manucho254 <manuchoadero@gmail.com>
2023-10-23 00:43:48 +0000

    . HBNB is alive!

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,22 +7,26 @@ from flask import render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
 def state_cities():
-    """ list state and all cities related to the state:
+    """
+       list state and all cities related to the state:
     """
 
-    states = storage.all(State).values()
+    states = sorted(storage.all(State).values(),
+                    key=lambda state: state.name)
 
     return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
 def tear_down(exception):
-    """ remove the current SQLAlchemy Session
+    """
+       remove the current SQLAlchemy Session
     """
     storage.close()
 
